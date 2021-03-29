809fa7c2b079d258761d8c94f6afb950f2a57178
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-03-29 20:41:04 +0200

    log response

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
-
   const [, x, y, z] = pathname.split('/');
-
   const apiKey = Deno.env.get('API_KEY');
-
-  console.log(apiKey, { x, y, z });
 
   return fetch(
     `https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/${apiKey}/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topowebb&STYLE=default&TILEMATRIXSET=3857&TILEMATRIX=${z}&TILEROW=${y}&TILECOL=${x}&FORMAT=image/png`
@@ -14,6 +10,8 @@ async function handleRequest(request) {
 
 addEventListener('fetch', async event => {
   const response = await handleRequest(event.request);
+
+  console.log(response);
 
   event.respondWith(response);
 });
