1babfe2ece0e67c76877407065a7ac4ac18b63d9
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-03-29 20:35:25 +0200

    Try adding lantmäteriet req

--- index.js
+++ index.js
@@ -1,55 +1,15 @@
-function handleRequest(request) {
+async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`;
+  const [, x, y, z] = pathname.split('/');
 
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        "content-type": "text/html; charset=UTF-8",
-      },
-    });
-  }
+  const apiKey = Deno.env.get('API_KEY');
 
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-
-  return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=UTF-8",
-      },
-    },
+  const response = await fetch(
+    `https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/${apiKey}/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topowebb&STYLE=default&TILEMATRIXSET=3857&TILEMATRIX=${z}&TILEROW=${y}&TILECOL=${x}&FORMAT=image/png`
   );
+
+  return response;
 }
 
 addEventListener("fetch", (event) => {
